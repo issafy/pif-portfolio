1e39d1b0f90f986882b3d85f4f68a70ffeb2073c
Pap. I. FY <skywalker7102pif@gmail.com>
2023-04-10 19:24:34 +0000

    Updated tailwind configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,15 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   mode: "jit",
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        primary: "#050816",
-        secondary: "#aaa6c3",
-        tertiary: "#151030",
+        primary: "#DFEEEA",
+        secondary: "#2f5d62",
+        tertiary: "#A7C4BC",
+        primary_dark: "#051505",
+        secondary_dark: "#a9c3a7",
+        tertiary_dark: "#113216",
         "black-100": "#100d25",
         "black-200": "#090325",
         "white-100": "#f3f3f3",
+        "green-1000": "#002106",
+        
       },
       boxShadow: {
         card: "0px 35px 120px -15px #211e35",
@@ -19,7 +25,8 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('/src/assets/herobg.png')",
+        "hero-pattern_light": "url('/src/assets/herobg_light.png')",
+        "hero-pattern_dark": "url('/src/assets/herobg_dark.png')",
       },
     },
   },
