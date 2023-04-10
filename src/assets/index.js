e01141ff73dc645c304c5d970a87ffdfaa9b2ec0
Pap. I. FY <skywalker7102pif@gmail.com>
2023-04-10 20:48:38 +0000

    Major push v2

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,8 +6,6 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import sun from "./sun.svg";
-import moon from "./moon-fill.svg";
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
@@ -41,8 +39,6 @@ export {
   github,
   menu,
   close,
-  sun,
-  moon,
   css,
   docker,
   figma,
