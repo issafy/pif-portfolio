00d2fd0214bcc17ef6fbf34d8ba2efb7b6091fd3
Pap. I. FY <skywalker7102pif@gmail.com>
2023-04-24 12:08:16 +0000

    Email setup

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,7 +29,7 @@ const Contact = () => {
   };
 
   
-  console.log("Env Test " + process.env.VITE_APP_EMAILJS_PUBLIC_KEY);
+  // console.log("Env Test " + process.env.VITE_APP_EMAILJS_PUBLIC_KEY);
   
 
   const handleSubmit = (e) => {
@@ -42,9 +42,9 @@ const Contact = () => {
         process.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "P. Issa FAYE",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "papify02@gmail.com",
           message: form.message,
         },
         process.env.VITE_APP_EMAILJS_PUBLIC_KEY
